03f9dc8c258e443b0e9f4f3321d5e9e042c04d41
ToniGracanin <tonigracanin6@gmail.com>
2024-12-01 20:11:02 -0600

    12/1/24 personal website

---
+++ resumejs.js
@@ -0,0 +1,9 @@
+function openresume(){
+	var resume = 'resume.png';
+	window.open(resume, '_blank');
+}
+
+function openportfolio(){
+	var portfolio = 'portfolio.html';
+	window.open(portfolio, '_blank');
+}
